be50d119ec15cae71bfe657b8a3c7dfea0f4b3b6
Mihal Ciko <mihalciko18@gmail.com>
2023-06-15 13:34:29 +0200

    First Commit

---
+++ src/main/java/com/mcode/alumninetwork3i/AlumniNetwork3iApplication.java
@@ -0,0 +1,13 @@
+package com.mcode.alumninetwork3i;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class AlumniNetwork3iApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(AlumniNetwork3iApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/mcode/alumninetwork3i/AlumniNetwork3iApplicationTests.java
@@ -0,0 +1,13 @@
+package com.mcode.alumninetwork3i;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class AlumniNetwork3iApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
