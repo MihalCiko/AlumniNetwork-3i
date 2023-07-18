a08accd840127a0e2ce221e5c9872dfc873d1608
Mihal Ciko <mihalciko18@gmail.com>
2023-07-18 09:28:43 +0200

    Changes in EventController

--- src/main/java/com/mcode/alumninetwork3i/controller/EventController.java
+++ src/main/java/com/mcode/alumninetwork3i/controller/EventController.java
@@ -2,10 +2,7 @@ package com.mcode.alumninetwork3i.controller;
 
 import com.mcode.alumninetwork3i.dto.eventDto.EventDto;
 import com.mcode.alumninetwork3i.dto.eventDto.EventGetDto;
-import com.mcode.alumninetwork3i.dto.userDto.UserDto;
-import com.mcode.alumninetwork3i.dto.userDto.UserGetDto;
 import com.mcode.alumninetwork3i.service.eventService.EventService;
-import com.mcode.alumninetwork3i.service.userService.UserService;
 import lombok.RequiredArgsConstructor;
 import org.springframework.web.bind.annotation.*;
 
@@ -21,29 +18,29 @@ public class EventController {
 
 
     @PostMapping
-    public EventGetDto save(@RequestBody EventDto eventDto){
+    public EventGetDto save(@RequestBody EventDto eventDto) {
         return eventService.save(eventDto);
     }
 
     @GetMapping
-    public List<EventGetDto> getAll(){
+    public List<EventGetDto> getAll() {
         return eventService.getAll();
     }
-    @GetMapping("{eventid}")
-    public EventGetDto getById(@PathVariable UUID eventid){
-        return eventService.getById(eventid);
-    }
-    @PatchMapping("{eventid}")
-    public EventGetDto update(@PathVariable UUID eventid, @RequestBody EventDto eventDto){
-        return eventService.update(eventid,eventDto);
+
+    @GetMapping("{eventId}")
+    public EventGetDto getById(@PathVariable UUID eventId) {
+        return eventService.getById(eventId);
     }
 
-    @DeleteMapping("{eventid}")
-    public void delete (@PathVariable UUID eventid){
-        eventService.delete(eventid);
+    @PatchMapping("{eventId}")
+    public EventGetDto update(@PathVariable UUID eventId, @RequestBody EventDto eventDto) {
+        return eventService.update(eventId, eventDto);
     }
 
-
+    @DeleteMapping("{eventId}")
+    public void delete(@PathVariable UUID eventId) {
+        eventService.delete(eventId);
+    }
 
 
 }
